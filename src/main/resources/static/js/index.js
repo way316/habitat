1ba8f4f50188f1eee1df28151e301cb7d2f47f3d
enze-cont <elii0014@student.monash.edu>
2022-04-06 14:37:50 +1000

    position to location

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -44,6 +44,42 @@ function initMap(data) {
 			lng: 144
 		},
 	});
+	const geocoder = new google.maps.Geocoder();
+	const infowindow = new google.maps.InfoWindow();
+
+	document.getElementById("submit").addEventListener("click", () => {
+		geocodeLatLng(geocoder, map, infowindow);
+	});
+}
+
+function geocodeLatLng(geocoder, map, infowindow) {
+	const input = document.getElementById("latlng").value;
+	const latlngStr = input.split(",", 2);
+	const latlng = {
+		lat: parseFloat(latlngStr[0]),
+		lng: parseFloat(latlngStr[1]),
+	};
+
+	geocoder
+		.geocode({
+			location: latlng
+		})
+		.then((response) => {
+			if (response.results[0]) {
+				map.setZoom(11);
+
+				const marker = new google.maps.Marker({
+					position: latlng,
+					map: map,
+				});
+
+				infowindow.setContent(response.results[0].formatted_address);
+				infowindow.open(map, marker);
+			} else {
+				window.alert("No results found");
+			}
+		})
+		.catch((e) => window.alert("Geocoder failed due to: " + e));
 
 	let infoWindow = new google.maps.InfoWindow({
 		content: "Click the map to get Lat/Lng!",
